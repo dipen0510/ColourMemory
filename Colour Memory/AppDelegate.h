e0a506e6b42c34838de6668ebaa1216006b27dd5
Dipen Sekhsaria <dipensekhsaria@dipens-macbook.local>
2017-02-26 19:51:28 +0530

    Initial Commit

---
+++ Colour Memory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Colour Memory
+//
+//  Created by Dipen Sekhsaria on 26/02/17.
+//  Copyright © 2017 Dipen Sekhsaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Colour Memory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Colour Memory
+//
+//  Created by Dipen Sekhsaria on 26/02/17.
+//  Copyright © 2017 Dipen Sekhsaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
